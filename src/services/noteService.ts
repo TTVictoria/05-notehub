21d492835e42824d97a2cc8a72e64b2565459c8b
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2026-02-07 13:45:44 +0100

    noteServise.ts

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Note } from '../types/note';
+import type { Note } from '../types/note';
 
 const api = axios.create({
   baseURL: 'https://notehub-public.goit.study/api',
