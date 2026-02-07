6abbde0329b9aeb828fac2b1d9795032f28d5b7e
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2026-02-07 13:39:25 +0100

    App_css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./components/App/App";
+import 'modern-normalize';
+
+
+
 
 const queryClient = new QueryClient();
 
