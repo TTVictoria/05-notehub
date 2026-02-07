c2b3a48426f7359a3cfc2390dea9fcfad5dcc6cf
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2026-02-07 13:29:37 +0100

    main

---
+++ src/services/noteService.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { Note } from '../types/note';
+
+const api = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`,
+  },
+});
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export const fetchNotes = async (page: number, perPage: number, search?: string) => {
+  const { data } = await api.get<FetchNotesResponse>('/notes', {
+    params: { page, perPage, search },
+  });
+  return data;
+};
+
+export const createNote = async (note: Omit<Note, 'id' | 'createdAt' | 'updatedAt'>) => {
+  const { data } = await api.post<Note>('/notes', note);
+  return data;
+};
+
+export const deleteNote = async (id: string) => {
+  const { data } = await api.delete<Note>(`/notes/${id}`);
+  return data;
+};
