74c7ba356fd44dcc16f1df09e648e991d8ffbfc1
TTVictoria <146879934+TTVictoria@users.noreply.github.com>
2026-02-08 18:08:40 +0100

    main

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -9,26 +9,27 @@ interface NoteListProps {
 
 export default function NoteList({ notes }: NoteListProps) {
   const queryClient = useQueryClient();
+
   const { mutate } = useMutation({
     mutationFn: (id: number) => deleteNote(id),
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["note"] }),
   });
+
   return (
-    <>
-      <ul className={css.list}>
-        {notes.map((note: Note) => (
-          <li className={css.listItem} key={note.id}>
-            <h2 className={css.title}>{note.title}</h2>
-            <p className={css.content}>{note.content}</p>
-            <div className={css.footer}>
-              <span className={css.tag}>{note.tag}</span>
-              <button className={css.button} onClick={() => mutate(note.id)}>
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className={css.list}>
+      {notes.map((note: Note) => (
+        <li className={css.listItem} key={note.id}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            {}
+            <button className={css.button} onClick={() => mutate(Number(note.id))}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
